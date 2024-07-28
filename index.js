22c2ca6b80b720e1e64f9adcd8bbb6140f2fcd51
Thalles1946 <thallesaug2099@gmail.com>
2024-07-28 10:52:00 -0300

    feat : Creation of get history path

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 8000;
+const mockDataPath = "./data/mock-data.json";
+
+app.get("/api/get-history", (req, res) => {
+  try {
+    let obj;
+    fs.readFile(mockDataPath, "utf-8", function (err, data) {
+      if (err) throw err;
+      obj = JSON.parse(data);
+      const responseArray = obj.data.historic;
+      res.send(responseArray);
+    });
+  } catch (error) {
+    res.send("Erro ao ler arquivo mock-data : " + error);
+  }
+});
 
 app.listen(port, () => {
   console.log("Application running at port " + port);
